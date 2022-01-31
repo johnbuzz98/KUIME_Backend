34a7ad65da839dfc5be758d56bc20e6eb899d612
WooJun <woojoonlee98@gmail.com>
2022-01-31 19:05:25 +0900

    CRUD class updated, flask_config revised

--- app/__init__.py
+++ app/__init__.py
@@ -9,4 +9,22 @@ def create_app():
     return app
 
 
+# flask_login에서 제공하는 login_required를 실행하기 전 사용자 정보를 조회한다.
+@login_manager.user_loader
+def user_loader(user_id):
+    # 사용자 정보 조회
+    user_info = User.get_user_info(user_id)
+    # user_loader함수 반환값은 사용자 '객체'여야 한다.
+    # 결과값이 dict이므로 객체를 새로 생성한다.
+    login_info = User(user_id=user_info['data'][0]['USER_ID'])
+
+    return login_info
+
+
+# login_required로 요청된 기능에서 현재 사용자가 로그인되어 있지 않은 경우
+# unauthorized 함수를 실행한다.
+@login_manager.unauthorized_handler
+def unauthorized():
+    # 로그인되어 있지 않은 사용자일 경우 첫화면으로 이동
+    return redirect("/")
 
