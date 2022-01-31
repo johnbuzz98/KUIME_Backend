5be41c0a0b5ab6040cf6a41c63e1348ea3d0cf1e
WooJun <woojoonlee98@gmail.com>
2022-01-31 17:09:30 +0900

    flask_config.py Update

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
-import unittest
+from flask import Flask
+from flask_login import LoginManager
+
+def create_app():
+    # 앱 설정
+    app = Flask(__name__)
+    login_manager = LoginManager()
+    login_manager.init_app(app)  # app 에 login_manager 연결
+    return app
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
 
-
-if __name__ == '__main__':
-    unittest.main()
