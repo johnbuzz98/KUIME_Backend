e10bdf8095c9ae8a61ab6c63738ec17500d5c7be
Woo Jun Lee <52363901+johnbuzz98@users.noreply.github.com>
2022-02-09 15:24:42 +0900

    login module added

--- app/__init__.py
+++ app/__init__.py
@@ -10,3 +10,9 @@ def create_app():
 
 
 
+
+# @login_required (로그인이 필요한 기능 앞에다 staticmethod 형식으로 선언해주면 됨
+# 해당방식은 세션 유지 정보를 통해 로그인된 상태인지 확인 (보안성 조금 떨어짐)
+
+# is.authenticated로 구현하면 직접 확인하는 방식으로도 가능
+# current_user.is_authenticated
